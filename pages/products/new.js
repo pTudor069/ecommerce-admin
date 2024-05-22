ff78c017fa932c01e876f9329ba433baa04fd717
Puiu Tudor <puiu.tudor024@gmail.com>
2024-05-22 17:30:27 +0100

    Verdsion 2

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/Layout";
 
 export default function NewProduct() {
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
+    const [price,setPrice] = useState('');
     return (
         <Layout>
+            <h1>New Product</h1>
+            <label>Product Name</label>
             <input type="text" placeholder="product name"/>
+            <label>Description</label>
+            <textarea placeholder="description"></textarea>
+            <label>Price (in EURO)</label>
+            <input type="number" placeholder="price"></input>
+            <button className="btn-primary">Save</button>
         </Layout>
     );
 }
